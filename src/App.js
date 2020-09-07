dabee112594e47d85b632a17fd0c20e87e92acd3
Selfmade20 <tebogo1686@gmail.com>
2020-09-07 15:33:17 +0200

    My programming portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Tebogo Gift",
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' },
+      ],
+      home: {
+        title: "Be Relentless",
+        subTitle: 'Projects that make a difference',
+        text: 'Checkout my projects'
+      },
+      about: {
+        title: "About Me",
+      },
+      contact: {
+        title: "Let's talk",
+      },
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <Container>
+
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
