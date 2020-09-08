24e6ceb7b3c18789b372f8daaf0c23365cab119d
Selfmade20 <tebogo1686@gmail.com>
2020-09-08 11:39:03 +0200

    Importing the footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import Footer from './components/Footer';
 
 class App extends Component {
   constructor(props) {
@@ -42,6 +43,7 @@ class App extends Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+          <Footer />
         </Container>
       </Router>
     );
