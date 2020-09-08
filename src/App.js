262de84de3be79c0bc95294c2de14e30c01a9807
Selfmade20 <tebogo1686@gmail.com>
2020-09-08 09:58:20 +0200

    creating a website navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 class App extends Component {
   constructor(props) {
@@ -28,8 +30,18 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Container>
-
+        <Container className='p-0' fluid={true}>
+          <Navbar className='border-bottom' bg='transparent' expand='lg'>
+            <Navbar.Brand>Tebogo Gift</Navbar.Brand>
+            <Navbar.Toggle className='border-0' aria-controls='navbar-toggle' />
+            <Navbar.Collapse id='navbar-toggle'>
+              <Nav className='ml-auto'>
+                <Link className='nav-link' to='/'>Home</Link>
+                <Link className='nav-link' to='/about'>About</Link>
+                <Link className='nav-link' to='/contact'>Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         </Container>
       </Router>
     );
