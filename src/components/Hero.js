437c74bf88acaca14186c0310bce1ce23fa678ee
Selfmade20 <tebogo1686@gmail.com>
2020-09-08 12:56:20 +0200

    Homepages content container

---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function Hero(props) {
+    console.log("props" , props)
+  return (
+    <Jumbotron>
+      <Container>
+        <Row>
+          <Col>
+            {props.title && <h1>{props.title}</h1>}
+            {props.isubTitle && <h3>{props.subTitle}</h3>}
+            {props.text && <h3>{props.text}</h3>}
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+  );
+}
+
+export default Hero;
