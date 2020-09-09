1a8b3e9b48bb764a2d9a3a1b487f35508b7c616c
Selfmade20 <tebogo1686@gmail.com>
2020-09-09 11:16:00 +0200

    Accessing props to display

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from './Hero'
 
-function Home() {
+function Home(props) {
     return (
         <div>
-            Homepage works
+            <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
         </div>
     )
 }
